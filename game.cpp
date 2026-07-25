c5a163a9e8dfd8c1fa9e5fc78634ffa3fdb1faa2
app-specialist-11 <9@felixau.in>
2026-07-25 15:17:14 +0530

    Implement features in game.cpp - Version 4

--- game.cpp
+++ game.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
+int main() {
+    std::srand(std::time(0));
+    int secret = std::rand() % 100 + 1;
+    int guess = 0;
+    int attempts = 0;
